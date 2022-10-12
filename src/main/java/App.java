0a13fe6687795137a0236d80ad676fa35defc47a
Aleksey Kochetkov <alexey2606@gmail.com>
2022-10-12 15:00:43 +0300

    add: Cat class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,20 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld anotherBean = applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println("Равны ли ссылки для bean и anotherBean? -- " + (bean == anotherBean));
+        System.out.println("--------------");
+
+
+        Cat catFirst = applicationContext.getBean("cat", Cat.class);
+        Cat catSecond = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println("Равны ли ссылки для catFirst и catSecond? -- " + (catFirst == catSecond));
+        System.out.println(catFirst + " -- ссылка catFirst");
+        System.out.println(catSecond + " -- ссылка catSecond");
     }
 }
